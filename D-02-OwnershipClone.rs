1e835b3b31bc9096f471e75edcf09fdb37a55ac6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 15:24:32 +0300

    Create D-02-OwnershipClone.rs

---
+++ D-02-OwnershipClone.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    //clone!
+    
+    let name = String::from("Mustafa");
+    
+    let name2 = name.clone();
+    
+    println!("My name is {}.", name); //My name is Mustafa. 
+
+    println!("My name is {}.", name2); //My name is Mustafa.
+}
