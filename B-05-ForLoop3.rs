ccaea467c792ed2bde88ab849e36a4b255c7cff9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 12:47:30 +0300

    Create B-05-ForLoop3.rs

---
+++ B-05-ForLoop3.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    //break
+    for counter in 2..12 {
+        if counter == 7 {break}
+        println!("Counter value: {}", counter);
+    }
+
+}
+
+/*
+Counter value: 2
+Counter value: 3
+Counter value: 4
+Counter value: 5
+Counter value: 6
+*/
