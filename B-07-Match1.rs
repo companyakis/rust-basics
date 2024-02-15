69b3b7213264f73ec5747ecaa9c4c386841f388f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 13:27:23 +0300

    Create B-07-Match1.rs

---
+++ B-07-Match1.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    let langs_i_use: u8 = 4;
+    
+    match langs_i_use {
+        
+        1 => println!("Rust"),
+        2 => println!("Python"),
+        3 => println!("Dart"),
+        4 => println!("MQL5"),
+        5 => println!("Solidity"),
+        6 => println!("Javascript"),
+        7 => println!("Move"),
+        _ => println!("I don't use!"),
+    }
+
+}
+
+/*
+MQL5
+*/
