9d5f42f301605db8f941d2ca96abd43c3b9e2aa6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 13:05:05 +0300

    Create B-05-ForLoop6.rs

---
+++ B-05-ForLoop6.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let chars: [char; 7] = ['m', 'u', 's', 't', 'a', 'f', 'a'];
+    
+    print!("My name is: ");
+    
+    for c in chars {
+        print!{"{}", c};
+    }
+
+}
+
+/*
+My name is: mustafa
+
+*/
