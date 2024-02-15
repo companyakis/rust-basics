e4e9f67b3b6b6489f6867e8a1758153ee1a32e17
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 12:34:49 +0300

    Create B-04-While.rs

---
+++ B-04-While.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    let mut counter: u8 = 0;
+    
+    while counter < 11 {
+        println!("Counter value: {}", counter);
+        counter += 1;
+    }
+
+}
+
+/*
+Counter value: 0
+Counter value: 1
+Counter value: 2
+Counter value: 3
+Counter value: 4
+Counter value: 5
+Counter value: 6
+Counter value: 7
+Counter value: 8
+Counter value: 9
+Counter value: 10
+*/
