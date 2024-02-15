e6a23355f6d5de6c3cce4a0dbef339599ffe6d96
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 12:55:19 +0300

    Create B-05-ForLoop5.rs

---
+++ B-05-ForLoop5.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    //enumerate
+    for (index, counter) in (2..12).enumerate() {
+        
+        println!("Counter_{}: {}", index, counter);
+    }
+
+}
+
+/*
+Counter_0: 2
+Counter_1: 3
+Counter_2: 4
+Counter_3: 5
+Counter_4: 6
+Counter_5: 7
+Counter_6: 8
+Counter_7: 9
+Counter_8: 10
+Counter_9: 11
+*/
