c93fa857bed1ebc03e5016876ce38923962b60d7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 12:52:28 +0300

    Create B-05-ForLoop4.rs

---
+++ B-05-ForLoop4.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    //reversed
+    for counter in (2..12).rev() {
+        
+        println!("Reversed counter value: {}", counter);
+    }
+
+}
+
+/*
+Reversed counter value: 11
+Reversed counter value: 10
+Reversed counter value: 9
+Reversed counter value: 8
+Reversed counter value: 7
+Reversed counter value: 6
+Reversed counter value: 5
+Reversed counter value: 4
+Reversed counter value: 3
+Reversed counter value: 2
+*/
