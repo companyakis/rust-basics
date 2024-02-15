9d7c1bd8da4ca5199a6f9676912d63e64113cd32
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 14:37:00 +0300

    Create C-04-ClosuresAnonymousFunctions1.rs

---
+++ C-04-ClosuresAnonymousFunctions1.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let sum = |a: i32, b: i32, c: i32| -> i32 {
+        a + b + c
+    };
+    
+    println!("Sum of the numbers is: {}", sum(12, -5, 14));
+
+}
+
+
+/*
+Sum of the numbers is: 21
+*/
