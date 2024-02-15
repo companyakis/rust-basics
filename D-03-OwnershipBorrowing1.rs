ca4a53d74cee1fb1611e274610cbefc919d68d4e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 15:35:32 +0300

    Create D-03-OwnershipBorrowing1.rs

---
+++ D-03-OwnershipBorrowing1.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    //clone!
+    
+    let name = "Mustafa".to_string();
+    
+    let name2 = &name;
+    
+    println!("My name is {}.", name); //My name is Mustafa. 
+
+    println!("My name is {}.", name2); //My name is Mustafa.
+}
