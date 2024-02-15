118510164d0b150a7bf450bb1acf68c98f489a92
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 21:36:40 +0300

    Create E-03-StringConcatenation1.rs

---
+++ E-03-StringConcatenation1.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let name = String::from("Mustafa");
+    
+    let surname = String::from(" Buyukdereli");
+    
+    //let me = name + surname; //expected `&str`, found `String` 
+    
+    let me = name + &surname;
+    
+    println!("{}", me); //Mustafa Buyukdereli
+
+}
