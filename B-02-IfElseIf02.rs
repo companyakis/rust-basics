3ba09902897c61923e2aa0a7ecaee66841d4a4bd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 12:15:41 +0300

    Create B-02-IfElseIf02.rs

---
+++ B-02-IfElseIf02.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let balance_ethereum: u8 = 140;
+    
+    if balance_ethereum >= 10 || balance_ethereum < 50 {
+        println!("Good!");
+    } else if balance_ethereum >= 50 && balance_ethereum < 100 {
+        println!("Very good!");
+    } else {
+        println!("We should check and decide...");
+    }
+}
+
+//Good!
+//Why? Because we used ||
