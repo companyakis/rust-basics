8c01719aa1d87200533ac97890edfb1c381d3c1e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 15:19:45 +0300

    Create D-01-Ownership2.rs

---
+++ D-01-Ownership2.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+    //we should understand what heap and stack are!
+    
+    //be careful! stack!
+    
+    let year = 2024;
+    
+    let number = year;
+    
+    println!("year: {}", year); 
+
+    println!("number: {}", number); 
+}
+
+
+/*
+year: 2024
+number: 2024
+*/
