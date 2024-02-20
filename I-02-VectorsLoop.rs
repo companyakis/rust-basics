f98f9a3caa97591877da0d10354cd8ad23be9fc8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-20 09:50:07 +0300

    Create I-02-VectorsLoop.rs

---
+++ I-02-VectorsLoop.rs
@@ -0,0 +1,17 @@
+#[allow(unused_variables)]
+
+fn main() {
+
+    let v2 = vec![100, 200, 300, 400, 500];
+    
+    for i in v2 {
+        print!("{:?} ", i);
+    }
+
+}
+
+
+/*
+100 200 300 400 500 
+*/
+
