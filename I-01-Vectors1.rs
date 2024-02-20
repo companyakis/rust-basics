ec6caa732575df10893e1fb1708a71bab9340814
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-20 09:40:42 +0300

    Create I-01-Vectors1.rs

---
+++ I-01-Vectors1.rs
@@ -0,0 +1,12 @@
+#[allow(unused_variables)]
+
+fn main() {
+
+    let v1: Vec<u8> = Vec::new();
+    
+    let mut v2 = vec![100, 200, 300];
+    
+    let v3: Vec<i16> = vec![-10, -345, 0, 12];
+    
+    let names: Vec<&str> = vec!["Mustafa", "Kutluk", "Bilge", "Bumin", "Kültigin"];
+}
