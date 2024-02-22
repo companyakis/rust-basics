cd4315dec35e45a8776ef1d8aa16cc34c33c20fc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-22 10:58:46 +0300

    Create M-01-HashMap1.rs

---
+++ M-01-HashMap1.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+#[allow(unused_variables)]
+
+
+fn main() {
+
+    //keys and values
+    //name and age 
+
+    let mut children = HashMap::new();
+    
+    children.insert("Casper", 752);
+    children.insert("Sylvester", 7);
+    children.insert("Tweety", 3);
+    children.insert("Sylvester Jr", 3);
+
+}
