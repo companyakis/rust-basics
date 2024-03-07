7f9a661b2aa17694ca90c8f8a8f383149b0348a7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-07 19:01:29 +0300

    Create A-04-BasicDataTypes2.rs

---
+++ A-04-BasicDataTypes2.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+    //min and max values of integer types
+
+    //examples
+
+    let min_val_i16 = i16::MIN;
+
+    let max_val_16 = i16::MAX;
+
+    let min_val_u32 = u32::MIN;
+
+    let max_val_u32 = u32::MAX;
+
+    println!("min_val_i16 : {min_val_i16}");
+    println!("max_val_16 : {max_val_16}" );
+    println!("min_val_u32 : {min_val_u32}");
+    println!("max_val_u32 : {max_val_u32}" );
+
+}
+
+/*
+min_val_i16 : -32768
+max_val_16 : 32767
+min_val_u32 : 0
+max_val_u32 : 4294967295
+
+*/
