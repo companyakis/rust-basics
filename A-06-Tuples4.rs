373f8ca237ccff4a1ddf8f20695afeddbfd0f791
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-07 19:09:11 +0300

    Create A-06-Tuples4.rs

---
+++ A-06-Tuples4.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+//nested tuples
+
+let aboutHer = (("Aysel", "Göktürk"), 29, true);
+
+let name = aboutHer.0.0;
+
+let surname = aboutHer.0.1;
+
+println!("{name} {surname}"); //Aysel Göktürk
+
+}
+
